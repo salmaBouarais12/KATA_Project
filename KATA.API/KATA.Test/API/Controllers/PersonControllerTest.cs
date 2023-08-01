2b3e88b504470fead7eebed1e775bcc74bd6fdd6
SalmaBouarais <salmabouarais@fuwear-technology.com>
2023-08-01 11:19:28 +0200

    Add Test unitaire

---
+++ KATA.API/KATA.Test/API/Controllers/PersonControllerTest.cs
@@ -0,0 +1,42 @@
+﻿using KATA.API.Controllers;
+using KATA.API.DTO.Responses;
+using KATA.Domain.Interfaces.Sevices;
+using KATA.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+using NFluent;
+using NSubstitute;
+
+namespace KATA.Test.API.Controllers;
+
+public class PersonControllerTest
+{
+    [Fact]
+    public async Task Should_Get_PersonById()
+    {
+        //Arrange
+        var Person = new Person { Id = 8, FirstName = "Salma", LastName = "BOUARAIS" };
+        IPersonService personService = Substitute.For<IPersonService>();
+        personService.GetPersonByIdAsync(Arg.Is<int>(x => x == Person.Id)).Returns(Person);
+        //personService.GetPersonByIdAsync(Arg.Any<int>()).Returns(Person);
+
+        var personController = new PersonController(personService);
+        var personById = await personController.Get(Person.Id);
+        Check.That(personById).IsNotNull();
+        Check.That(personById).IsInstanceOf<OkObjectResult>();
+
+        Check.That(((OkObjectResult)personById).Value).IsNotNull();
+        Check.That(((OkObjectResult)personById).Value).IsInstanceOf<PersonResponse>();
+        Check.That(((PersonResponse)((OkObjectResult)personById).Value!).Id).IsEqualTo(8);
+    }
+    [Fact]
+    public async Task Should_Get_Error404_WhenPersonDoesNotExist()
+    {
+        IPersonService personService = Substitute.For<IPersonService>();
+
+        var personController = new PersonController(personService);
+        var result = await personController.Get(default);
+
+        Check.That(result).IsNotNull();
+        Check.That(result).IsInstanceOf<NotFoundResult>();
+    }
+}
---
+++ KATA.API/KATA.Test/Domain/Services/PersonServiceTest.cs
@@ -0,0 +1,31 @@
+﻿using KATA.Domain.Interfaces.Repositories;
+using KATA.Domain.Models;
+using KATA.Domain.Services;
+using NFluent;
+using NSubstitute;
+using NSubstitute.Core;
+
+namespace KATA.Test.Domain.Services;
+
+public class PersonServiceTest
+{
+    [Fact]
+    public async Task Should_Get_AllPersons()
+    {
+        //Arrange
+        var listPersons = new List<Person>
+        {
+            new Person { Id = 8,FirstName = "Salma", LastName = "BOUARAIS" },
+            new Person {Id = 9 , FirstName = "Kevin" , LastName = "Sousselier"}
+        };
+        IPersonRepository personRepository = Substitute.For<IPersonRepository>();
+        personRepository.GetAllPersonsAsync().Returns(listPersons);
+        var personService = new PersonService(personRepository);
+
+        //Act
+        var allPersons = await (personService.GetAllPersonsAsync());  
+        
+        //Assert
+        Check.That(allPersons).HasSize(2);
+    }
+}
